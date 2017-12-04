ca2a3b2fbef6fb314b1fbe15b2a505126a9d6a53
Simon Hutchinson <simonhutc@gmail.com>
2017-12-04 22:05:24 +1100

    "firebase done"

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,52 +1,172 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyD8GfQtIzq7HROjVpCWeKzLkYjZ-qqpmh4",
-    authDomain: "expensify-c97a4.firebaseapp.com",
-    databaseURL: "https://expensify-c97a4.firebaseio.com",
-    projectId: "expensify-c97a4",
-    storageBucket: "expensify-c97a4.appspot.com",
-    messagingSenderId: "611921736865"
+    apiKey: "AIzaSyB0MVdk3N6xgDb65-BSmfQwXIYR24YaFd8",
+    authDomain: "expenses4-8b624.firebaseapp.com",
+    databaseURL: "https://expenses4-8b624.firebaseio.com",
+    projectId: "expenses4-8b624",
+    storageBucket: "",
+    messagingSenderId: "720555727417"
   };
 
   firebase.initializeApp(config);
 
+ 
   const database = firebase.database();
 
-  database.ref().set({
 
-        name: 'Simon H',
-        age: 35,
-        isSingle: false,
-        location: {
-            city: 'Sydney',
-            country: 'AU'
-        }
+ database.ref('expenses').on('child_removed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
 
-  }).then(() => {
-  
-    console.log('data is saved');
 
-}).catch((e) => {
+database.ref('expenses').on('child_changed', (snapshot) => {
 
-console.log('this failed', e);
+  console.log(snapshot.key, snapshot.val());
 
 });
 
-  //database.ref().set('this is data');
 
-  //database.ref('age').set(27);
-  //database.ref('location/city').set('Brisbane');
-  //database.ref('attributes/height').set('180cm');
-  //database.ref('attributes/weight').set(80);
+database.ref('expenses').on('child_added', (snapshot) => {
 
-  database.ref('attributes').set({
-      height: 73,
-      weight: 80
-  }).then (() =>{
-        console.log('data saved');
+  console.log(snapshot.key, snapshot.val());
+
+});
+
+
+
+  // database.ref('expenses').on('value', (snapshot) => {
+  //   const expenses = [];
+  //   console.log(snapshot.val());
+  //   snapshot.forEach((childSnapshot) => {
+
+  //     expenses.push({
+
+  //       id: childSnapshot.key,
+  //       ...childSnapshot.val()
+
         
-  }).catch((e) => {
-    console.log ('error', e);
 
-  });
+  //     });
+
+  //   });
+
+  //   console.log(expenses);
+  // });
+
+
+  // database.ref('expenses').push({
+
+  //   description: 'Rent',
+  //   note: '',
+  //   amount: 109500,
+  //   createdAt: 94844985943935
+
+  // });
+
+
+
+
+  // const firebaseNotes = {
+  //   notes: {
+      
+  //   }
+  // }
+
+  // const notes = [{
+  //   id: '12',
+  //   title: 'first note',
+  //   body: 'testing this out'
+  // }, {
+
+  //   id: '712',
+  //   title: 'second note',
+  //   body: 'testing this out'
+  // }
+  
+  // ];
+
+  // database.ref('notes').set(notes);
+  // console.log(notes);
+
+
+//   database.ref().on('value', (snapshot) => {
+
+//     console.log(snapshot.val());
+
+//   });
+
+// setTimeout (() => {
+//   database.ref('age').set(28);
+// }, 3500);
+
+// setTimeout (() => {
+//   database.ref('age').set(28);
+// }, 7000);
+
+// setTimeout (() => {
+//   database.ref('age').set(28);
+// }, 10500);
+
+
+
+  // database.ref('')
+  // .once('value')
+  // .then ((snapshot)=> {
+  //   const val = snapshot.val();
+  //   console.log(val);
+
+  // }).catch((e)=> {
+
+  //   console.log('error', e);
+
+  // });
+
+
+
+// //database.child('isSingle').remove();
+
+// database.ref('isSingle')
+// .remove()
+// .then (() => {
+
+// console.log('data removed');
+
+// }).catch((e) => {
+
+// console.log('error occured', e);
+
+// });
+
+// database.ref().update({
+
+//   name: 'Darren S',
+//   age: 34,
+//   job: 'Contracts Admin',
+//   isSingle: null
+
+// });
+
+
+
+
+//   database.ref().set({
+
+//         name: 'Simon H',
+//         age: 35,
+//         isSingle: false,
+//         location: {
+//             city: 'Sydney',
+//             country: 'AU'
+//         }
+
+//   }).then(() => {
+  
+//     console.log('data is saved');
+
+// }).catch((e) => {
+
+// console.log('this failed', e);
+
+// });
+
