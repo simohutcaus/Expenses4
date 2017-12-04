9bfbb303a792e0db268aa252dae5638ee48a1dc0
simohutcaus <Simon.Hutchinson@reckon.com>
2017-12-04 15:44:33 +1100

    test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -23,11 +23,30 @@ const config = {
             country: 'AU'
         }
 
-  });
+  }).then(() => {
+  
+    console.log('data is saved');
+
+}).catch((e) => {
+
+console.log('this failed', e);
+
+});
 
   //database.ref().set('this is data');
 
-  database.ref('age').set(27);
-  database.ref('location/city').set('Brisbane');
-  database.ref('attributes/height').set('180cm');
-  database.ref('attributes/weight').set(80);
+  //database.ref('age').set(27);
+  //database.ref('location/city').set('Brisbane');
+  //database.ref('attributes/height').set('180cm');
+  //database.ref('attributes/weight').set(80);
+
+  database.ref('attributes').set({
+      height: 73,
+      weight: 80
+  }).then (() =>{
+        console.log('data saved');
+        
+  }).catch((e) => {
+    console.log ('error', e);
+
+  });
