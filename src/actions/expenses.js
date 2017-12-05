12b974600d27f4aff5cc1a7f753ec61812d4a2a2
Simon Hutchinson <Simon.Hutchinson@reckon.com>
2017-12-05 16:41:30 +1100

    database done

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -7,8 +7,6 @@ export const addExpense = (expense) => ({
   expense
 });
 
-
-
 export const startAddExpense = (expenseData = {}) => {
     return (dispatch) => {
 
@@ -50,3 +48,38 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+
+
+// SET_EXPENSES
+
+export const setExpenses = (expenses) => ({
+
+  type:'SET_EXPENSES',
+  expenses
+});
+
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+     return database.ref('expenses').once('value').then((snapshot)=> {
+
+      const expenses = [];
+      snapshot.forEach((childSnapshot) => {
+
+        expenses.push({
+
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+
+      });
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
+
+
+
+
+
