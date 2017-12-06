c9486cd924c63e1fff8df7dc7c2057399c79b384
simohutcaus <Simon.Hutchinson@reckon.com>
2017-12-06 17:18:09 +1100

    auth

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
@@ -13,6 +13,17 @@ import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
+
+let hasRendered = false;
+const renderApp = () => {
+
+if (!hasRendered) {
+  ReactDOM.render(jsx, document.getElementById('app'));
+  hasRendered = true;
+}
+
+};
+
 const jsx = (
   <Provider store={store}>
     <AppRouter />
@@ -21,14 +32,20 @@ const jsx = (
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-});
-
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     console.log('log in');
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp()
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+
   } else {
+    renderApp();
     console.log('log out');
+    history.push('/');
+    console.log('pushed');
   }
 });
